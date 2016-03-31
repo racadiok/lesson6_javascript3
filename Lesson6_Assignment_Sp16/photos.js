d61b573e886c8cd7fbf8ba5c9cfeaef4388170f3
racadiok <kr15b@my.fsu.edu>
2016-03-31 00:21:51 +0000

    Lesson 6 final commit

--- Lesson6_Assignment_Sp16/photos.js
+++ Lesson6_Assignment_Sp16/photos.js
@@ -64,41 +64,39 @@ function leftArrow() {
 
 function previewFive() {
 
-var articleEl = document.getElementsByTagName("article")[0];
+   var articleEl = document.getElementsByTagName("article")[0];
 
-// create figure and img elements for fifth image var lastFigure = document.createElement("figure");
+   // create figure and img elements for fifth image var lastFigure = document.createElement("figure");
 
-lastFigure.id = "fig5";
-lastFigure.style.zIndex = "5";
-lastFigure.style.position = "absolute";
-lastFigure.style.right = "45px";
-lastFigure.style.top = "67px";
+   var lastFigure = document.createElement("figure");
+   
+   lastFigure.id = "fig5";
+   lastFigure.style.zIndex = "5";
+   lastFigure.style.position = "absolute";
+   lastFigure.style.right = "45px";
+   lastFigure.style.top = "67px";
+   
+   var lastImage = document.createElement("img"); lastImage.width = "240";
+   lastImage.height = "135";
+   lastFigure.appendChild(lastImage);
+   articleEl.appendChild(lastFigure);
 
-var lastImage = document.createElement("img");
-lastImage.width = "240";
-lastImage.height = "135";
+   //clone figure element for fifth image and edit to be first image var firstFigure = lastFigure.cloneNode(true);
+   
+   var firstFigure = lastFigure.cloneNode(true)
+   
+   firstFigure.id = "fig1";
+   firstFigure.style.right = " ";
+   firstFigure.style.left = "45px";
 
-lastFigure.appendChild(lastImage);
-
-articleEl.appendChild(lastFigure);
-
-//clone figure element for fifth image and edit to be first image var firstFigure = lastFigure.cloneNode(true);
-
-firstFigure.id = "fig1";
-firstFigure.style.right = " ";
-firstFigure.style.left = "45px";
-
-articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
-
-// add appropriate src values to two new img elements
-
-document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
-document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
-
-figureCount = 5;
-
-//disable the ‘Show more images’ button after it has been selected once var numberButton = document.querySelector("#fiveButton p"); numberButton.removeEventListener("click", previewFive, false);
-
+   articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
+   
+   // add appropriate src values to two new img elements
+   document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg"; document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+   
+   figureCount = 5;
+   
+   //disable the ‘Show more images’ button after it has been selected once var numberButton = document.querySelector("#fiveButton p"); numberButton.removeEventListener("click", previewFive, false);
 
 }//end of previewFive()
 
